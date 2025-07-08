78218433ae355cacf2e25912cd930a155dbe31e2
Daniel P.B. <daniel.perezbaltazar@nttdata.com>
2025-07-08 12:54:42 -0600

    endpoint para validar token de usuario logeado

--- src/main/java/com/nttdata/loginultra/controller/AdminController.java
+++ src/main/java/com/nttdata/loginultra/controller/AdminController.java
@@ -1,28 +1,34 @@
 package com.nttdata.loginultra.controller;
 
-import com.nttdata.loginultra.utils.JwtUtils;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/admin")
-@PreAuthorize("hasRole('Admin')")
+@RequestMapping("/api")
 public class AdminController {
 
-    @GetMapping()
-    public Map<String, Object> adminData(Authentication authentication) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("acceso", "Acceso administrador FX Ultra");
+    @GetMapping("/validate-token")
+    public ResponseEntity<Map<String, Object>> adminData(Authentication authentication) {
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        String userName = jwt.getClaimAsString("name");
+        List<String> roles = jwt.getClaimAsStringList("roles"); // Roles extraídos por Spring Security
 
-        JwtUtils.addUserInfoToResponse(authentication, response);
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("message", "¡Usuario autenticado y validado por el backend!");
+        responseBody.put("userName", userName != null ? userName : "N/A");
+        responseBody.put("roles", roles != null ? roles : Collections.emptyList()); // <-- Envía los roles aquí
 
-        return response;
+        return ResponseEntity.ok(responseBody);
+
     }
 
 }
